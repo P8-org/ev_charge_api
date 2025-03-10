265f8dd4a1814400428a5a26d09b0655bc710bc1
Nistec <julian.larsen07@gmail.com>
2025-03-10 14:08:27 +0100

    api kald

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from typing import Union
-
 from fastapi import FastAPI
+import requests
 
 app = FastAPI()
 
@@ -9,7 +9,14 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
+@app.get("/power")
+def power():
+    return requests.get('https://api.energidataservice.dk/dataset/DeclarationProduction?start=2022-05-01&end=2022-06-01&filter={"PriceArea":["DK1"]}').json()
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+
+
