98167e5bc1060e7240f6629b48cd62ad3843fde6
Nistec <julian.larsen07@gmail.com>
2025-04-07 10:52:41 +0200

    yet another hotfix for ailab

--- models/run.sh
+++ models/run.sh
@@ -7,5 +7,8 @@
 #SBATCH --cpus-per-task=15      # CPUs per task
 #SBATCH --gres=gpu:3            # Allocated GPUs
 #SBATCH --time=12:00:00         # Maximum run time
- 
+
+singularity exec /ceph/container/python/python_3.10.sif pip install --user virtualenv
+singularity exec /ceph/container/python/python_3.10.sif ~/.local/bin/virtualenv ~/my-virtual-env
+
 singularity exec --bind /my-virtual-env:/my-virtual-env /ceph/container/python/python_3.10.sif /bin/bash -c "source /my-virtual-env/bin/activate && python3 '/ceph/project/charge_buddy/actions-runner/_work/ev_charge_api/ev_charge_api/run_RL.py'"
