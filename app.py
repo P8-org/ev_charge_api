4f1e831d5ada8adbaa3b227073d8b8e5cbbaf56c
APaludan <andreashp@outlook.com>
2025-04-15 11:18:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -38,31 +38,24 @@ def read_root():
 @app.get("/power")
 def power(): # should be replaced, but proof-of-concept
 
-    # e = EnergiData()
-    # now = datetime.date(year=2024,month=4,day=18)
-    # last_week = datetime.date(year=2024,month=4,day=17)
-    # lim = 24
-    # option = "HourUTC,PriceArea"
-    # fil = json.dumps({"PriceArea": ["DK1"]})
-    # sort = "HourUTC"
-    # offset = 0
-
-    # # rd = RequestDetail(startDate=last_week, endDate=now, dataset="Elspotprices", optional=option, limit=lim, filter_json=fil, sort_data=sort, offset=offset)
-    # # rd = RequestDetail(startDate=last_week, endDate=now,dataset="Elspotprices",limit=lim, filter_json=fil)
-    # # rd = RequestDetail(startDate=last_week, endDate=now,dataset="Elspotprices", optional=option, filter_json=fil)
-    # rd = RequestDetail(startDate=last_week, endDate=now,dataset="Elspotprices", filter_json=fil)
-    # # e.call_api(rd)
-    # # return e.data
-    # return e.get_today(rd)
-    formatted_time = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M")
     e = EnergiData()
-    rd = RequestDetail(startDate=formatted_time, dataset="Elspotprices", filter_json=json.dumps({"PriceArea": ["DK1"]}), sort_data="HourDK ASC")
-    response = e.call_api(rd)
-    hour_dk = [record.HourDK for record in response]
-    prices = [record.SpotPriceDKK / 1000 for record in response]
-    return [{"time": h, "price": p} for h, p in zip(hour_dk, prices)]
+    now = datetime.date(year=2024,month=4,day=18)
+    last_week = datetime.date(year=2024,month=4,day=17)
+    lim = 24
+    option = "HourUTC,PriceArea"
+    fil = json.dumps({"PriceArea": ["DK1"]})
+    sort = "HourUTC"
+    offset = 0
 
-@app.get("/rl_schedule") # should also be replaces. also proof of concept :)
+    # rd = RequestDetail(startDate=last_week, endDate=now, dataset="Elspotprices", optional=option, limit=lim, filter_json=fil, sort_data=sort, offset=offset)
+    # rd = RequestDetail(startDate=last_week, endDate=now,dataset="Elspotprices",limit=lim, filter_json=fil)
+    # rd = RequestDetail(startDate=last_week, endDate=now,dataset="Elspotprices", optional=option, filter_json=fil)
+    rd = RequestDetail(startDate=last_week, endDate=now,dataset="Elspotprices", filter_json=fil)
+    # e.call_api(rd)
+    # return e.data
+    return e.get_today(rd)
+
+@app.get("/rl_schedule") # should also be replaced. also proof of concept :)
 def schedule(num_hours: int, battery_level: float, battery_capacity: float, max_chargin_rate: float):
     formatted_time = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M")
     e = EnergiData()
