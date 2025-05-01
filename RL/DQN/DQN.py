a2e7f60552cbe4ec9a3ab3b5a4ce4ba983391b4d
Nistec <julian.larsen07@gmail.com>
2025-05-01 15:44:50 +0200

    cleanup and prepare for big model

--- RL/DQN/DQN.py
+++ RL/DQN/DQN.py
@@ -332,26 +332,27 @@ def train_agent(env, agent, num_episodes=500, print_iter=False):
 def run_dqn():
     """Runs the training and testing process for the electric charging environment."""
     cars = [
-        {'id': 0, 'charged': False, 'charge': 0, 'charge_percentage': 0, 'max_charge': 80, 'charge_speed': 22, 'constraints': {"start": 10, "end": 17}},
-        {'id': 1, 'charged': False, 'charge': 40, 'charge_percentage': 0, 'max_charge': 60, 'charge_speed': 22, 'constraints': {"start": 2, "end": 5}},
-        {'id': 2, 'charged': False, 'charge': 0, 'charge_percentage': 0, 'max_charge': 60, 'charge_speed': 10, 'constraints': {"start":12}},
-        {'id': 3, 'charged': False, 'charge': 20, 'charge_percentage': 0, 'max_charge': 80, 'charge_speed': 15, 'constraints': {"end":14}},
-        {'id': 4, 'charged': False, 'charge': 0, 'charge_percentage': 0, 'max_charge': 60, 'charge_speed': 22, 'constraints': {}},
-        {'id': 5, 'charged': False, 'charge': 20, 'charge_percentage': 0, 'max_charge': 60, 'charge_speed': 12, 'constraints': {}},
+        {'id': 0, 'charged': False, 'charge': 0, 'charge_percentage': 0, 'max_charge': 80, 'charge_speed': 22, 'constraints': {}},
+        # {'id': 0, 'charged': False, 'charge': 0, 'charge_percentage': 0, 'max_charge': 80, 'charge_speed': 22, 'constraints': {"start": 10, "end": 17}},
+        # {'id': 1, 'charged': False, 'charge': 40, 'charge_percentage': 0, 'max_charge': 60, 'charge_speed': 22, 'constraints': {"start": 2, "end": 5}},
+        # {'id': 2, 'charged': False, 'charge': 0, 'charge_percentage': 0, 'max_charge': 60, 'charge_speed': 10, 'constraints': {"start":12}},
+        # {'id': 3, 'charged': False, 'charge': 20, 'charge_percentage': 0, 'max_charge': 80, 'charge_speed': 15, 'constraints': {"end":14}},
+        # {'id': 4, 'charged': False, 'charge': 0, 'charge_percentage': 0, 'max_charge': 60, 'charge_speed': 22, 'constraints': {}},
+        # {'id': 5, 'charged': False, 'charge': 20, 'charge_percentage': 0, 'max_charge': 60, 'charge_speed': 12, 'constraints': {}},
     ]
     num_chargers = len(cars)
     # num_episodes = 500
     num_episodes = 2000
     rd = RequestDetail(
-        # startDate="2023-12-31T13:00",
-        # endDate="2024-12-31T12:00",
-        startDate="2024-04-30T13:00",
+        startDate="2023-12-31T13:00",
         endDate="2024-12-31T12:00",
+        # startDate="2024-04-30T13:00",
+        # endDate="2024-12-31T12:00",
         dataset="Elspotprices",
         # optional="HourDK,SpotPriceDKK",
         sort_data="HourDK ASC",
         filter_json=json.dumps({"PriceArea": ["DK1"]}),
-        limit=24*5, # Default=0, to limit set to a minimum of 72 hours
+        limit=24*0, # Default=0, to limit set to a minimum of 72 hours
         offset=0
     )
     data = EnergiData().call_api(rd)
@@ -428,24 +429,4 @@ def run_dqn():
         start_time_str = start_time_dt.strftime("%Y-%m-%d %H:%M")
         print(f"At {start_time_str} (Price: {prices_48[hour_index]:.2f}) -> Charged: {car['car_id']} (Hour {car['start_time']}) Charge Time: {car['duration']}")
 
-    print(env.schedule)
-
-    # print(prices_48)
-    # print("\n")
-    #
-    # start = cars[0]['constraints']['start']
-    # end = cars[0]['constraints']['end']
-    # print(prices_48[start:end])
-    # start = cars[1]['constraints']['start']
-    # end = cars[1]['constraints']['end']
-    # print(prices_48[start:end])
-    # print("\n")
-    # start = cars[2]['constraints']['start']
-    # print(prices_48[start:])
-    # end = cars[3]['constraints']['end']
-    # print(prices_48[:end])
-
-    # env.render()
-    # print(times_48)
-    # print(charging_curves)
-    # return env.schedule
+    print(env.schedule)    
