17ce99c381c7a54e670b9e3c9408efab2e3028ce
mpmunch <mpmunch@gmail.com>
2025-05-02 23:07:11 +0200

    add constraint management: support updating and deleting constraints

--- routers/constraints.py
+++ routers/constraints.py
@@ -10,7 +10,8 @@ from routers.schedules import make_schedule
 router = APIRouter()
 
 class ConstraintForm(BaseModel):
-    start_time: datetime.datetime = datetime.datetime.now()
+    id: int | None = None
+    startTime: datetime.datetime = datetime.datetime.now()
     deadline: datetime.datetime = datetime.datetime.now() + datetime.timedelta(days=1)
     target_percentage: float = 0.8
 
@@ -25,7 +26,7 @@ async def edit_constraint(ev_id: int, form: ConstraintForm, db: Session = Depend
     if ev is None:
         raise HTTPException(status_code=404, detail=f"EV with id {ev_id} not found")
 
-    start_time = form.start_time
+    start_time = form.startTime
     deadline = form.deadline
     target_percentage = form.target_percentage
 
@@ -35,6 +36,16 @@ async def edit_constraint(ev_id: int, form: ConstraintForm, db: Session = Depend
         raise HTTPException(status_code=400, detail="Deadline must be in the future")
     if start_time >= deadline:
         raise HTTPException(status_code=400, detail="Starttime must be before deadline")
+
+    if form.id is not None:
+        # Try to find existing constraint
+        constraint = db.query(Constraint).filter_by(id=form.id, ev_id=ev_id).first()
+        if constraint:
+            constraint.start_time = form.startTime
+            constraint.charged_by = form.deadline
+            constraint.target_percentage = form.target_percentage
+            db.commit()
+            return {"detail": f"Constraint {constraint.id} updated"}
 
     new_constraint = Constraint(
         start_time=start_time,
@@ -46,5 +57,17 @@ async def edit_constraint(ev_id: int, form: ConstraintForm, db: Session = Depend
     db.commit()
     db.refresh(new_constraint)
 
-    db.commit()
+    return {"detail": f"Constraint {new_constraint.id} created"}
+
+    # db.commit()
     # await make_schedule(ev_id, db)
+
+# Føler det burde være en post med ("/constraints/{id}/delete) men idk
+@router.delete("/constraints/{constraint_id}")
+async def delete_constraint(constraint_id: int, db: Session = Depends(get_db)):
+    constraint = db.query(Constraint).filter_by(id=constraint_id).first()
+    if constraint is None:
+        raise HTTPException(status_code=404, detail="Constraint not found")
+    db.delete(constraint)
+    db.commit()
+    return {"detail": f"Constraint {constraint_id} deleted"}
