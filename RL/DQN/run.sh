1058ab7c571766b760fa72da941b55b8dcbcd7cc
Nistec <julian.larsen07@gmail.com>
2025-05-05 10:47:29 +0200

    replaced wait for queue with wait for file

--- RL/DQN/run.sh
+++ RL/DQN/run.sh
@@ -1,16 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 #SBATCH --job-name=test  # Name of your job
+#SBATCH --signal=B:SIGTERM@30
 #SBATCH --output=test-out.out     # Name of the output file
 #SBATCH --error=file-test.err # Name of the error file
-#SBATCH --mem=86G               # Memory
-#SBATCH --cpus-per-task=64      # CPUs per task
-#SBATCH --gres=gpu:5            # Allocated GPUs
-#SBATCH --time=00:02:00         # Maximum run time
+#SBATCH --mem=64G               # Memory
+#SBATCH --cpus-per-task=32      # CPUs per task
+#SBATCH --gres=gpu:2            # Allocated GPUs
+#SBATCH --time=00:05:00         # Maximum run time
+#SBATCH --requeue
 
 #######################################################################################
 # tweak this to fit your needs
-max_restarts=4
+max_restarts=20
 
 # Fetch the current restarts value from the job context
 scontext=$(scontrol show job ${SLURM_JOB_ID})
@@ -48,4 +50,4 @@ trap 'term_handler' SIGTERM
 
 #######################################################################################
 
-singularity exec --bind ~/my-virtual-env:/my-virtual-env /ceph/container/python/python_3.10.sif /bin/bash -c "source /my-virtual-env/bin/activate && python3 '/ceph/project/charge_buddy/actions-runner/_work/ev_charge_api/ev_charge_api/run_DQN.py'"
+srun --output="${outfile}" --error="${errfile}" singularity exec --bind ~/my-virtual-env:/my-virtual-env /ceph/container/python/python_3.10.sif /bin/bash -c "source /my-virtual-env/bin/activate && python3 '/ceph/project/charge_buddy/actions-runner/_work/ev_charge_api/ev_charge_api/run_DQN.py'"
