ef0dd98e06f4eeb626325e82e745e6265dba7603
Nistec <julian.larsen07@gmail.com>
2025-05-07 13:12:56 +0200

    remove train limit

--- run_DQN.py
+++ run_DQN.py
@@ -7,8 +7,8 @@ import json
 #Only created since root folder should be in path(python is weird)
 
 cars = [
-    # {'id': 0, 'charge_percentage': 0, 'min_percentage': 50, 'charge': 0, 'max_charge_kw': 80, 'charge_speed': 22, 'constraints': {}},
-    {'id': 0, 'charge_percentage': 0, 'min_percentage': 80, 'charge': 0, 'max_charge_kw': 80, 'charge_speed': 22, 'constraints': {"start": 15, "end": 17}},
+    {'id': 0, 'charge_percentage': 0, 'min_percentage': 50, 'charge': 0, 'max_charge_kw': 80, 'charge_speed': 22, 'constraints': {}},
+    # {'id': 0, 'charge_percentage': 0, 'min_percentage': 80, 'charge': 0, 'max_charge_kw': 80, 'charge_speed': 22, 'constraints': {"start": 15, "end": 17}},
     # {'id': 0, 'charge_percentage': 0, 'min_percentage': 80, 'charge': 0, 'max_charge_kw': 80, 'charge_speed': 22, 'constraints': {"start": 14, "end": 17}},
     # {'id': 0, 'charge_percentage': 0, 'min_percentage': 80, 'charge': 0, 'max_charge_kw': 80, 'charge_speed': 22, 'constraints': {"start": 12, "end": 17}},
     # {'id': 0, 'charge_percentage': 0, 'min_percentage': 80, 'charge': 0, 'max_charge_kw': 80, 'charge_speed': 22, 'constraints': {"start": 7, "end": 17}},
@@ -32,7 +32,7 @@ rd = RequestDetail(
     # optional="HourDK,SpotPriceDKK",
     sort_data="HourDK ASC",
     filter_json=json.dumps({"PriceArea": ["DK1"]}),
-    limit=24*5, # Default=0, to limit set to a minimum of 72 hours
+    limit=24*0, # Default=0, to limit set to a minimum of 72 hours
     offset=0
 )
 train_dqn(cars=cars,rd=rd,num_chargers=num_chargers,num_episodes=num_episodes)
